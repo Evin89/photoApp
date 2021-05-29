7e574a06f7ff1e8b5f6fdc5a71aa540b1a442187
Evin Wijninga <e.d.wijninga@gmail.com>
2021-05-29 11:38:02 +0200

    breeze, tinker, migration
    Added breeze
    Added tinker
    
    photo first migration

---
+++ resources/views/photo/index.blade.php
@@ -0,0 +1,3 @@
+<html>
+    <h1>Photo</h1>
+</html>
---
+++ resources/views/photos/index.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <h1>Photos</h1>
+    <ul>
+        <li>Photo 1</li>
+        <li>Photo 2</li>
+        <li>Photo 3</li>
+        <li>Photo 4</li>
+        <li>Photo 5</li>
+        <li>Photo 6</li>
+
+    </ul>
+</html>
