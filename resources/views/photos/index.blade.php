15aee170523f83f9ac403817dcb0f6c89536846c
Evin Wijninga <e.d.wijninga@gmail.com>
2021-05-31 18:50:18 +0200

    Added routes
    Added routes

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -1,12 +1,17 @@
-<html>
+
+@extends('layouts.app')
+@section('content')
+    <p>This is my body content.</p>
     <h1>Photos</h1>
     <ul>
-        <li>Photo 1</li>
-        <li>Photo 2</li>
-        <li>Photo 3</li>
-        <li>Photo 4</li>
-        <li>Photo 5</li>
-        <li>Photo 6</li>
+        <li><a href="photo/1">Photo 1</a></li>
+        <li><a href="photo/2">Photo 2</a></li>
+        <li><a href="photo/3">Photo 3</a></li>
+        <li><a href="photo/4">Photo 4</a></li>
+        <li><a href="photo/5">Photo 5</a></li>
+        <li><a href="photo/6">Photo 6</a></li>
 
     </ul>
+@endsection
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ImageUploadController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\PhotosController;
+use App\Http\Controllers\CommentController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,11 +19,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// photos routes
+Route::get('/photos', [PhotosController::class, 'index']);
 
+// photo routes
+Route::post('/photo', [PhotoController::class, 'create']);
+Route::get('/photo{id}', [PhotoController::class, 'index']);
+Route::edit('/photo{id}', [PhotoController::class, 'edit']);
+Route::update('/photo{id}', [PhotoController::class, 'update']);
+Route::delete('/photo{id}', [PhotoController::class, 'delete']);
+
+// comment routes
+Route::post('/comment', [CommentController::class, 'create']);
+Route::get('/comment{id}', [ComCommentController::class, 'index']);
+Route::edit('/comment{id}', [CommentController::class, 'edit']);
+Route::update('/comment{id}', [CommentController::class, 'update']);
+Route::delete('/comment{id}', [CommentController::class, 'delete']);
+
+Route::get('/dashboard', function () { return view('dashboard');})->middleware(['auth'])->name('dashboard');
 require __DIR__.'/auth.php';
+
+// users routes
+// Route::get('/users', function(){ return view('users.index'); });
+
+// user routes
+Route::get('/user/{id}', function(){return view('user.index');});
+
